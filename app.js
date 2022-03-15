22a93010a11c61d609fab40109274555a2fbf70a
Leifer Mendez <leifer33@gmail.com>
2022-03-15 19:38:28 +0100

    update

--- app.js
+++ app.js
@@ -15,7 +15,9 @@ const NODE_ENV = process.env.NODE_ENV || 'development';
 app.use(cors());
 app.use(express.json());
 app.use(express.static("storage"));
-
+/**
+ * 
+ */
 morganBody(app, {
   noColors: true,
   stream: loggerStream,
